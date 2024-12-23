76fa36dad003b90d474e76a60e1a741dd8f42248
Saifahmeed <saif_ahmed@aucegypt.edu>
2024-12-23 05:29:16 +0200

    Libraries

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieCloud.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+
+    }
+}
